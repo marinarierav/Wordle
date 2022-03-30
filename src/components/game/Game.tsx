cef41d9664e1e70855db70bb3d5e21b419d291e6
marinarierav <marina.rierav@gmail.com>
2022-03-30 22:59:04 +0200

    feat: use seed to refresh words once per day

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -2,87 +2,90 @@ import React from "react";
 import AddWordForm from "./AddWordForm";
 import Word from "./Word";
 import GameOverModal from "./modal/GameOverModal";
+import seedrandom from "seedrandom";
 
-const WORDS = {
-  4: [
-    "Chad",
-    "Cuba",
-    "Fiji",
-    "Iran",
-    "Iraq",
-    "Laos",
-    "Mali",
-    "Niue",
-    "Oman",
-    "Peru",
-    "Togo",
-  ],
-  5: [
-    "Benin",
-    "Egypt",
-    "Gabon",
-    "Ghana",
-    "Kenya",
-    "Libya",
-    "Niger",
-    "Sudan",
-    "China",
-    "India",
-    "Japan",
-    "Nepal",
-    "Qatar",
-    "Syria",
-    "Yemen",
-    "Nauru",
-    "Palau",
-    "Samoa",
-    "Tonga",
-    "Italy",
-    "Malta",
-    "Spain",
-  ],
-  6: [
-    "Angola",
-    "Belize",
-    "Bhutan",
-    "Brazil",
-    "Brunei",
-    "Canada",
-    "Cyprus",
-    "France",
-    "Gambia",
-    "Greece",
-    "Guinea",
-    "Guyana",
-    "Israel",
-    "Jordan",
-    "Kosovo",
-    "Kuwait",
-    "Latvia",
-    "Malawi",
-    "Mexico",
-    "Monaco",
-    "Norway",
-    "Panama",
-    "Poland",
-    "Russia",
-    "Rwanda",
-    "Serbia",
-    "Sweden",
-    "Taiwan",
-    "Turkey",
-    "Tuvalu",
-    "Uganda",
-    "Zambia",
-  ],
-};
-export const DIFFICULTY = 4 + Math.floor(Math.random() * 3);
+const WORDS = [
+  //4
+  "Chad",
+  "Cuba",
+  "Fiji",
+  "Iran",
+  "Iraq",
+  "Laos",
+  "Mali",
+  "Niue",
+  "Oman",
+  "Peru",
+  "Togo",
+  //5
+  "Benin",
+  "Egypt",
+  "Gabon",
+  "Ghana",
+  "Kenya",
+  "Libya",
+  "Niger",
+  "Sudan",
+  "China",
+  "India",
+  "Japan",
+  "Nepal",
+  "Qatar",
+  "Syria",
+  "Yemen",
+  "Nauru",
+  "Palau",
+  "Samoa",
+  "Tonga",
+  "Italy",
+  "Malta",
+  "Spain",
+  //6
+  "Angola",
+  "Belize",
+  "Bhutan",
+  "Brazil",
+  "Brunei",
+  "Canada",
+  "Cyprus",
+  "France",
+  "Gambia",
+  "Greece",
+  "Guinea",
+  "Guyana",
+  "Israel",
+  "Jordan",
+  "Kosovo",
+  "Kuwait",
+  "Latvia",
+  "Malawi",
+  "Mexico",
+  "Monaco",
+  "Norway",
+  "Panama",
+  "Poland",
+  "Russia",
+  "Rwanda",
+  "Serbia",
+  "Sweden",
+  "Taiwan",
+  "Turkey",
+  "Tuvalu",
+  "Uganda",
+  "Zambia",
+];
+
+const seedForToday = Intl.DateTimeFormat("es-ES", {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+}).format(new Date());
+const rng = seedrandom(seedForToday);
+
 export const GROUNDTRUTH =
-  WORDS[DIFFICULTY][
-    Math.floor(Math.random() * WORDS[DIFFICULTY].length)
-  ].toUpperCase();
+  WORDS[Math.floor(rng() * WORDS.length)].toUpperCase();
 export const MAX_LETTERS = GROUNDTRUTH.length;
-export const MAX_WORDS = DIFFICULTY + 1;
+export const MAX_WORDS = MAX_LETTERS + 1;
 
 function Game() {
   console.log(GROUNDTRUTH);
