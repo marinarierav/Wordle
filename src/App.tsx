9129ef434de3b446ccda60f7915b0b93aab396d4
marinarierav <marina.rierav@gmail.com>
2022-05-28 21:29:57 +0200

    feat: adds settings menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,40 @@
+import AppMenu from "./components/AppMenu";
 import Game, { MAX_LETTERS } from "./components/Game";
-import GameContextProvider from "./components/Game/GameContext";
-import ValidLanguageWordButton from "./components/Game/ValidLanguageWordButton";
 
 function App() {
   return (
-    <GameContextProvider>
-      <main>
-        <div className="title">
-          <ul className="title--container">
-            <li className="title--box"></li>
-            <li>
-              <h1>Wordle</h1>
-            </li>
-            <li className="title--box">
-              <p>(English)</p>
-            </li>
-          </ul>
-          <h2>The popular daily word game</h2>
-        </div>
-        <div className="content game--container">
-          <p className="game--box">A different word every day</p>
-          <p className="game--box description">
-            Can you guess which word{" "}
-            <span className="description--highlight">
-              has {MAX_LETTERS} letters
-            </span>
-            ?
-          </p>
-          <Game></Game>
-          <ValidLanguageWordButton></ValidLanguageWordButton>
-        </div>
-        <div className="content">
-          <a href="https://github.com/marinarierav">
-            <button className="btn">Follow me on GitHub</button>
-          </a>
-        </div>
-      </main>
-    </GameContextProvider>
+    <main>
+      <div className="title">
+        <ul className="title--container">
+          <li className="title--box">
+            <AppMenu isModalOpenInitially={false}></AppMenu>
+          </li>
+          <li>
+            <h1 className="heading heading--title">Wordle</h1>
+          </li>
+          <li className="heading title--box">
+            <p>(English)</p>
+          </li>
+        </ul>
+        <h2 className="heading heading--desc">The popular daily word game</h2>
+      </div>
+      <div className="content game--container">
+        <p className="game--box">A different word every day</p>
+        <p className="game--box description">
+          Can you guess which word{" "}
+          <span className="description--highlight">
+            has {MAX_LETTERS} letters
+          </span>
+          ?
+        </p>
+        <Game></Game>
+      </div>
+      <div className="content">
+        <a href="https://github.com/marinarierav">
+          <button className="btn">Follow me on GitHub</button>
+        </a>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/AppMenu/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { WhatsappIcon, WhatsappShareButton } from "react-share";
+import Backdrop from "../common/Backdrop";
+import Modal from "../common/Modal";
+import { getShareableLink } from "../Game/GameOverModal/getShareable";
+import ValidLanguageWordButton from "../Game/ValidLanguageWordButton";
+
+function AppMenu({ isModalOpenInitially }): JSX.Element {
+  const [isModalOpen, setIsModalOpen] = React.useState(isModalOpenInitially);
+
+  function closeModalHandler(): void {
+    setIsModalOpen(false);
+  }
+
+  function openModalHandler(): void {
+    setIsModalOpen(true);
+  }
+
+  function reloadWindow(): void {
+    window.location.reload();
+  }
+
+  React.useEffect(() => {
+    setIsModalOpen(isModalOpenInitially);
+  }, [isModalOpenInitially]);
+
+  return (
+    <div className="title-menu">
+      <button onClick={openModalHandler}>
+        Menu
+        <hr className="title-menu--hr"></hr>
+        <span className="title-menu--icon">☰</span>
+      </button>
+      {isModalOpen && (
+        <Modal
+          onCancel={reloadWindow}
+          onCancelText="Retry"
+          onCancelExtraClasses="hidden"
+          onConfirm={closeModalHandler}
+          onConfirmText="Back to game"
+          onConfirmExtraClasses="btn--alt share--confirm"
+          title={"Settings"}
+        >
+          <ValidLanguageWordButton></ValidLanguageWordButton>
+          <p className="menu--separator">***</p>
+          <p className="share">How it works?</p>
+          <p className="">
+            You have 6 attemps to guess the 5-letter daily word. Use the
+            keyboard to enter a word. Once submitted, you will see:
+          </p>
+          <ul className="tutorial--list">
+            <li>In green: Letter is present and correctly placed.</li>
+            <li>In yellow: Letter is present but misplaced.</li>
+            <li>In white: Letter is not present.</li>
+          </ul>
+          <p>Come here every day for a different puzzle!</p>
+          <p className="menu--separator">***</p>
+          <p className="share">Invite your friends!</p>
+          <ul className="share--container">
+            <li className="">
+              <WhatsappShareButton
+                url={getShareableLink()}
+                title={"Can you solve this?"}
+              >
+                <WhatsappIcon size={32} round={true}></WhatsappIcon>
+              </WhatsappShareButton>
+            </li>
+          </ul>
+        </Modal>
+      )}
+      {isModalOpen && <Backdrop onClick={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default AppMenu;
